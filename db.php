87c49fe4698e02de7d2aa20b5a1f4ff0711427dd
MKE-JOE <60634251+MKE-JOE@users.noreply.github.com>
2024-06-12 13:33:28 -0500

    Update db.php

--- db.php
+++ db.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "sql.freedb.tech";
-$username = "freedb_EagleSD";
-$password = "M?H*vkC?eTGg$5h";
-$dbname = "freedb_eagle-sundance-messages";
+$servername = "j78.h.filess.io";
+$username = "eagleSD_stepbestme";
+$password = "ccfc30eef9497e073abb73ff14fd95661c54d98d";
+$dbname = "eagleSD_stepbestme";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
