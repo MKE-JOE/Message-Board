73a8720d56b4f87bbf1061b3c9fc204c5d0df973
MKE-JOE <60634251+MKE-JOE@users.noreply.github.com>
2024-06-12 13:45:57 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,3 @@
-<!---<<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$servername = "j78.h.filess.io";
-$username = "eagleSD_stepbestme";
-$password = "ccfc30eef9497e073abb73ff14fd95661c54d98d";
-$dbname = "eagleSD_stepbestme";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}--->
-
 <?php
 $servername = "j78.h.filess.io";
 $port = "3307";
